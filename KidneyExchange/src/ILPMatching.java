1472c55866c724b6c5a6feb00ea12cc57c0a3afe
martin-d-c <martindeclosets@yahoo.fr>
2021-02-05 19:18:20 +0100

    reste à tester ILP

--- KidneyExchange/src/ILPMatching.java
+++ KidneyExchange/src/ILPMatching.java
@@ -18,6 +18,8 @@ public class ILPMatching extends Matching {
 	public ILPMatching(String path) throws IOException {
 		graph = new DirectedCompatibilityGraph(path);
 		this.n = graph.n;
+		this.nbNotAssigned = graph.n;
+		this.notAssigned = graph.getVertices();
 	}
 	
 	int branchAndBound(double[][] A,double[] b,double[]c,ConsType[] rel,double bound, int interRes) throws Exception {
@@ -44,11 +46,11 @@ public class ILPMatching extends Matching {
            boolean isInteger =true;
            int index =0;
            for(Variable var:solution.getVariables()) {
-        	   if(Math.round(var.getValue()) == var.getValue()) {
-        		   index = Integer.parseInt(var.getName());
+        	   if(Math.round(var.getValue()) != var.getValue()) {
         		   isInteger = false;
         		   break;
         	   }
+        	   index++;
            }
            if(isInteger) {
         	   this.solutionILP = solution;
@@ -78,8 +80,50 @@ public class ILPMatching extends Matching {
         	return interRes;
         }
 	}
-	HashSet<Patient> match(){
-		LinkedList<LinkedList<Integer>> infeasiblePaths = this.graph.toId(this.graph.computeAllMinimalInfeasiblePaths(this.graph.K));
+	HashSet<Patient> match()  throws Exception{
+		LinkedList<LinkedList<Integer>> infeasiblePaths = DirectedCompatibilityGraph.toId(this.graph.computeAllMinimalInfeasiblePaths(this.graph.K));
+		int p = infeasiblePaths.size()+2*n;
+		
+		double[] c = new double[n*n];
+		for(int i = 0;i< (n*n);i++) { c[i] = 1; }
+		
+		double[][] A = new double[p][n*n];
+		double[] b = new double[p];
+		ConsType[] rel = new ConsType[p];
+		for(int i =1;i<n+1;i++) { 
+			for(int j =1;j<n+1;j++) {
+				if(this.graph.adjMatrix[i][j] ==1) { A[i-1][n*(i-1)+j-1] =1; A[n+j-1][n*(i-1)+j-1]=1;}
+			}
+			b[i-1]=1;
+			b[n+i-1]=1;
+			rel[i-1]=ConsType.LE;
+			rel[n+i-1]=ConsType.LE;
+		}
+		int k = 0;
+		for(LinkedList<Integer> L : infeasiblePaths) {
+			b[2*n+k] = this.graph.K;
+			rel[2*n+k] = ConsType.LE;
+			int prev = L.getFirst();
+			L.removeFirst();
+			for(int i :L) {
+				A[k+2*n][n*(prev-1)+i-1] =1;
+				prev=i;
+			}
+			k++;
+		}
+		
+		branchAndBound(A,b,c,rel,Double.POSITIVE_INFINITY,0);
+		int i=1,j =1;
+		for(Variable var : solutionILP.getVariables()) {
+			if(var.getValue() ==1) {
+				Patient P = new Patient(j,new boolean[n],new int [n]);
+				P.assign(i);
+				this.assigned.add(P);
+			}
+			
+			if(j==n) {i++;j=1;}
+			else {j++;}
+		}
 		return this.assigned;
 	}
 }
